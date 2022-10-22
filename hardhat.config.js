126de2a08aa1903060a3b55ab77b4b79ce5c5d4e
PlentyOfShade <eccouunlimited@gmail.com>
2022-10-22 12:13:01 -0400

    Unused packages+files cleanup

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,23 +4,44 @@ require("@nomiclabs/hardhat-web3");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
-  solidity: "0.8.10",
-
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      forking: {
-        url: process.env.ALCHEMY_MAINNET_RPC_URL,
-        accounts: {
-          mnemonic:
-            "test test test test test test test test test test test junk",
-          //path: "m/44'/60'/0'/0",
-          //initialIndex: 0,
-          //count: 20,
-          //passphrase: "",
-        },
+      // forking: {
+      //   url: process.env.ALCHEMY_MAINNET_RPC_URL,
+      //   // accounts: {
+      //   //   mnemonic:
+      //   //     "test test test test test test test test test test test junk",
+      //   //   path: "m/44'/60'/0'/0",
+      //   //   initialIndex: 0,
+      //   //   count: 20,
+      //   //   passphrase: "",
+      //   // },
+      // },
+    },
+  },
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
       },
     },
+  },
+  solidity: {
+    version: "0.8.10",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
   },
 };
